b595b77fdf5d7ca166b23c596208a098da308fd0
sophiatca <sophiatca@gmail.com>
2015-04-11 17:37:00 -0700

    Update read-command.c

--- read-command.c
+++ read-command.c
@@ -48,7 +48,7 @@ command_t make_simple_command (char* word_buffer[], command_t current_command, b
 		*words[c]= *word_buffer[c];
 	}
 	
-	current_command -> u.word = words[0];
+	current_command -> u.word = words;
 	return current_command;
 }
 
@@ -197,7 +197,7 @@ command_stream_t make_command_stream (int (*get_next_byte) (void *), void *get_n
 	command_stream_t root = (command_stream_t)malloc(sizeof(command_stream_t));
 	command_stream_t current_stream = (command_stream_t)malloc(sizeof(command_stream_t));
 
-	current_stream = root; 
+	current_stream-> next_command_stream = root; 
 
 	
 	enum command_type current_type = SIMPLE_COMMAND; //what is command_type? has it been declared?
@@ -217,95 +217,19 @@ command_stream_t make_command_stream (int (*get_next_byte) (void *), void *get_n
 
 	while (curr != EOF)
 	{
-		if (curr == '#' && (prev == ' ' || prev == '\n' || prev =='\t'))
-		{
-			while(curr != '\n' || curr  != EOF)
-			{
-				curr = read_char(get_next_byte, get_next_byte_argument);
-			}
-		}
+
 		if(is_valid (curr))
 		{
-			if(prev_prev =='\n' && prev =='\n')
-			{
-				//new command stream (new line)
-				command_stream_t new_stream = (command_stream_t)malloc(sizeof(command_stream_t));
-				current_stream->current_root_command = current_command;
-				current_stream->next_command_stream = new_stream;
-			}
-
-			if (prev =='&' && is_valid (prev_prev) )
-			{
-				//When you have only one '&'
-				fprintf(stderr, "%d: invalid operator. Must have two '&'.", lineNumber);
-				exit(1); 
-			}
-
-			if (is_valid(prev_prev) && prev =='\n')
-			{
-				// A \n B == A ; B
-				// A must be already in simple_command when the program reached '\n'
-				
-				current_command = make_complete_command(';', current_command);
-				push(current_command, command_stack, stack_size);
-				stack_size ++;
-			}
-
-			else 
-			{
-				//put characters in words
-				/*
-				if(nChars == wordsize) //NEED TO REALLOC!
-				{        
-					wordsize = wordsize*2;
-					word= (char*)realloc(word, wordsize);
-				}
-				*/
 				word[nChars] = curr;
 				nChars++;  
 				printf(nChars);
 				printf("\n");
-			}
+
 
 		}
 		else if( curr ==' ' && is_valid (prev))
 		{
 			//if white space after valid word
-
-			if( has_input )
-			{
-				if(nChars > wordsize)
-				{
-					wordsize = nChars;
-					input = (char*) realloc(input,wordsize);
-				}
-
-				strcpy(input,word);
-
-				while(nChars >= 0) //delete word or set everything to ''
-				{
-					word[nChars] = '0';
-					nChars--;
-				}
-			}
-			else if (has_output)
-			{
-				if(nChars > wordsize)
-				{
-					wordsize = nChars;
-					output = (char*) realloc(output,wordsize);
-					//
-				}
-				
-				strcpy(output, word);
-				
-				while(nChars >= 0) //delete word
-				{
-					word[nChars] = '0';
-					nChars--;
-				}
-			}
-			else
 			{
 				if(nChars >0)
 				{
@@ -328,215 +252,7 @@ command_stream_t make_command_stream (int (*get_next_byte) (void *), void *get_n
 
 
 		}
-		else if( curr== ';' ||  curr == '&' ||curr == '|' ||curr ==')')
-		{
-			if( (curr ==';' && prev ==';') || (curr =='&' && (prev !='&' || prev_prev =='&'))
-			                 || (curr =='|' && (prev =='|' || prev_prev =='|'))) 
-			{
-				// ;; , &&&, |||, invalid operators
-
-				fprintf(stderr, "%d: invalid operator", lineNumber);
-				exit(1);
-			}
-			if(prev == '\n')
-			{
-				//when newline starts with operators
-				fprintf(stderr, "%d: Cannot start a new line with operator", lineNumber);
-				exit(1);
-			}
-			if(curr == ';')
-			{
-				current_type = SEQUENCE_COMMAND;
-				if(nWords == 0 && !was_subshell)
-				{
-					fprintf(stderr, "%d: No LHS", lineNumber);
-					exit(1);
-				}
-			}
-			else if(curr == '&' && prev == '&')
-			{
-				current_type = AND_COMMAND;
-				if(nWords == 0 && !was_subshell)
-				{
-					fprintf(stderr, "%d: No LHS", lineNumber);
-					exit(1);
-				}
-			}
-			else if(curr == '|' && prev != '|')
-			{
-				current_type = PIPE_COMMAND;
-				if(nWords == 0  && !was_subshell)
-				{
-					fprintf(stderr, "%d: No LHS", lineNumber);
-					exit(1);
-				}
-			}
-			else if(curr == '|' && prev == '|')
-			{
-				current_type = OR_COMMAND;
-				if(nWords == 0 && (!was_subshell))
-				{
-					fprintf(stderr, "%d: No LHS", lineNumber);
-					exit(1);
-				}
-			}
-			else if (curr ==')')
-			{
-				current_type = SUBSHELL_COMMAND;
-
-				if(nWords == 0  && !was_subshell)
-				{
-					fprintf(stderr, "%d: No LHS", lineNumber);
-					exit(1);
-				}
-			} 
-
-			if( was_subshell && stack_size >0)
-			{
-				//if just now was a sibshell. prev ==')'
-				current_command = command_stack [stack_size-1]; //contain subshell command
-				pop(command_stack, stack_size);
-				stack_size --;
-				was_subshell= false;
-			}
-			else
-			{
-				// make simple command when you reached  '&&' '|' '||' ')'
-				// add everything in word buffer into simple command
-				//if there's input and output assign them too.
-				//copy last word to word_buffer
-
-				if (nChars >0)
-				{
-					//copy word to word_buffer
-
-					strcpy(word_buffer[nWords], word);
-					//strcpy (word_buffer[nWords],newword);
-					nWords ++;
-
-					while(nChars > 0) //delete word
-					{
-						word[nChars-1] = '0';
-						nChars--;
-					}
-
-				}
-
-				command_t new_simple_command = (command_t)malloc(sizeof(command_t));
-				current_command = make_simple_command(word_buffer, new_simple_command, has_input,has_output, input, output, nWords);
-				has_input = false;
-				has_output = false;
-				nWords =0;
-				//word_buffer = NULL:
-				// shoudl reset input, output to NULL here or inside make_simple_command
-			}
-
-
-
-			if(stack_size > 0)
-			{
-				//combining LHS!!!
-					
-				while(stack_size >0 && (compare_operator(current_type, command_stack[stack_size-1]->type) <= 0))
-				{
-					current_command = combine_complete_command(command_stack[stack_size-1], current_command);
-					pop(command_stack, stack_size);
-					stack_size--;
-				}
-
-				if( curr ==')')
-				{
-					current_command = combine_complete_command(current_command, command_stack[stack_size-1]);
-					push(current_command, command_stack, stack_size-1);
-					subshell_level --;
-					was_subshell =true;
-				}
-				else 
-				{
-					//if it's end of line, you won't need to start a new command
-					current_command = make_complete_command(curr, command_stack[stack_size-1]);
-					push(current_command, command_stack, stack_size-1);
-					//this push overwrites an entry; does not increase stack_size
-				}
-
-			}
-			else
-			{
-				//don't need to combine, just put the simple command into a complete command
-
-				current_command = make_complete_command(curr, current_command);
-				push(current_command, command_stack, stack_size);
-				stack_size++; 
-			}
-		}
-		else if(curr == '(')
-		{
-			if( curr =='(' && prev =='(') 
-			{
-				// '(('
-				fprintf(stderr, "%d: invalid operator, No '((' allowed ", lineNumber);
-				exit(1);
-			}
-			subshell_level ++;
-			current_command = make_complete_command(curr, current_command);
-			push (current_command, command_stack, stack_size);
-			stack_size ++;
-
-		}
-		else if (curr =='<')
-		{
-			if(prev == '\n')
-			{
-				fprintf(stderr, "%d: Cannot start a new line '<'", lineNumber);
-				exit(1);
-			}
-			if(!is_valid(prev))
-			{
-				fprintf(stderr, "%d: '<', input must have a output", lineNumber);
-				exit(1);
-			}
-			has_input = true;
-
-			//copy word to word_buffer
-
-					strcpy(word_buffer[nWords], word);
-					//strcpy (word_buffer[nWords],newword);
-					nWords ++;
-
-					while(nChars > 0) //delete word
-					{
-						word[nChars-1] = '0';
-						nChars--;
-					}
-		}
-		else if (curr == '>')
-		{
-			if(prev == '\n')
-			{
-				fprintf(stderr, "%d: Cannot start a new line '>'", lineNumber);
-				exit(1);
-			}
-			if(!is_valid(prev))
-			{
-				fprintf(stderr, "%d: '>', input must have a output", lineNumber);
-				exit(1);
-			}
-			has_output = true;
-
-			//copy word to word_buffer
-
-					strcpy(word_buffer[nWords], word);
-					//strcpy (word_buffer[nWords],newword);
-					nWords ++;
-
-					while(nChars > 0) //delete word
-					{
-						word[nChars-1] = '0';
-						nChars--;
-					}
-
-		}
-
+		
 		else if (curr == '\n')
 		{
 			//newline can not appear after '<' or '>'
@@ -550,8 +266,12 @@ command_stream_t make_command_stream (int (*get_next_byte) (void *), void *get_n
 
 				
 				//copy word to word_buffer
-
-					strcpy(word_buffer[nWords], word);
+				if(nChars >0)
+				{
+					//copy word to word_buffer
+					char newword[10];
+					strcpy(newword, word);
+					word_buffer[nWords] = newword;
 					//strcpy (word_buffer[nWords],newword);
 					nWords ++;
 
@@ -560,20 +280,14 @@ command_stream_t make_command_stream (int (*get_next_byte) (void *), void *get_n
 						word[nChars-1] = '0';
 						nChars--;
 					}
-				
+
+				}
 
 				command_t new_simple_command = (command_t)malloc(sizeof(command_t));
 				current_command = make_simple_command(word_buffer, new_simple_command, has_input,has_output, input, output, nWords);
 				has_input = false;
 				has_output = false;
 				nWords = 0;
-
-				while (stack_size >0)
-				{
-					current_command = combine_complete_command(command_stack[stack_size-1], current_command);
-					pop(command_stack, stack_size);
-					stack_size--;
-				}
 
 			}
 
@@ -612,11 +326,13 @@ command_stream_t make_command_stream (int (*get_next_byte) (void *), void *get_n
 			{
 
 				
-				//copy word to word_buffer
-
-					//strcpy(word_buffer[nWords], word);
+				if(nChars >0)
+				{
+					//copy word to word_buffer
+					char newword[10];
+					strcpy(newword, word);
+					word_buffer[nWords] = newword;
 					//strcpy (word_buffer[nWords],newword);
-					*word_buffer[nWords] = *word;
 					nWords ++;
 
 					while(nChars > 0) //delete word
@@ -624,6 +340,8 @@ command_stream_t make_command_stream (int (*get_next_byte) (void *), void *get_n
 						word[nChars-1] = '0';
 						nChars--;
 					}
+
+				}
 				
 
 				command_t new_simple_command = (command_t)malloc(sizeof(command_t));
@@ -631,13 +349,6 @@ command_stream_t make_command_stream (int (*get_next_byte) (void *), void *get_n
 				has_input = false;
 				has_output = false;
 				nWords = 0;
-/*
-				while (stack_size >0)
-				{
-					current_command = combine_complete_command(command_stack[stack_size-1], current_command);
-					pop(command_stack, stack_size);
-					stack_size--;
-				} */
 
 			}
 
@@ -664,12 +375,18 @@ command_stream_t make_command_stream (int (*get_next_byte) (void *), void *get_n
 	}
 
 	//when curr == 'EOF'
-	current_stream->current_root_command = current_command;
+	//when curr == 'EOF'
+	current_stream->next_command_stream -> current_root_command = current_command;
 
 
 
 	free (command_stack);
 	if( root == NULL)
+	{
+		fprintf (stderr, "NO COMMAND\n");
+		exit(1);
+	}
+	if( current_stream == NULL)
 	{
 		fprintf (stderr, "NO COMMAND\n");
 		exit(1);
