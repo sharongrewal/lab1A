18da184bd5423ac075bd0cc957028feccab0a6ff
sophiatca <sophiatca@gmail.com>
2015-04-12 08:15:46 -0700

    Update read-command.c

--- read-command.c
+++ read-command.c
@@ -777,6 +777,7 @@ command_stream_t make_command_stream (int (*get_next_byte) (void *), void *get_n
 		}
 		else
 		{
+			printf("root is not null");
 			current_stream -> next_command_stream = new_stream;
 			current_stream = current_stream->next_command_stream;
 		}
